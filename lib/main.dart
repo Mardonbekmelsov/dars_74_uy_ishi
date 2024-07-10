8cb79f83c6fe599916f604c5bd8c75f4d0bba65c
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-07-10 21:57:46 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:dars_14/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
